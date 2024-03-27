a7e1845f2a7421457ad08df13a8a4bc70bfccf6c
Noor El-Din Walid <52334140+iNoor72@users.noreply.github.com>
2024-03-27 23:33:02 +0200

    Initial Commit

---
+++ OrderApp-SwiftUI-TCA/OrderApp_SwiftUI_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderApp_SwiftUI_TCAApp.swift
+//  OrderApp-SwiftUI-TCA
+//
+//  Created by Noor El-Din Walid on 27/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct OrderApp_SwiftUI_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
